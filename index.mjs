351309a1bc823a8a1193bab11a2ee6d90fdb78dd
alexander-dobrin <alexander.dobrin@icloud.com>
2023-04-13 19:54:26 +0300

    feature: read doctors from fixture file

--- index.mjs
+++ index.mjs
@@ -6,3 +6,7 @@ const patients = JSON.parse(data);
 
 patients.forEach(user => console.log(user.firstName));
 
+const data2 = await fs.readFile(path.resolve('assets', 'doctors.json'), {encoding: 'utf8'});
+const doctors = JSON.parse(data2);
+
+doctors.forEach(doctor => console.log(doctor.speciality));
