3e13e679b1ffb16f0cc92c2ccc4ec61f296a219e
alexander-dobrin <alexander.dobrin@icloud.com>
2023-04-28 14:32:37 +0300

    fix: extend edge case handling for patient updating and deleting, recieve data from json

--- src/controllers/patients-controller.mjs
+++ src/controllers/patients-controller.mjs
@@ -30,15 +30,15 @@ export class PatientsController {
             const created = this.patientsService.create(req.body);
             res.status(STATUS_CODES.CREATED).json(created);
         } catch (err) {
+            if (err instanceof MissingParameterError) {
+                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
+                return;
+            }
             if (err instanceof InvalidParameterError) {
                 res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
                 return;
             }
             if (err instanceof DuplicateEntityError) {
-                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
-                return;
-            }
-            if (err instanceof MissingParameterError) {
                 res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
                 return;
             }
@@ -49,19 +49,50 @@ export class PatientsController {
 
     put(req, res) {
         try {
-            const updated = this.patientsService.update(req.params.phone, req.body);
+            req.body.oldPhone = req.params.phone;
+            const updated = this.patientsService.update(req.body);
+            if (!updated) {
+                res.sendStatus(STATUS_CODES.NOT_FOUND);
+                return;
+            }
             res.json(updated);
         } catch (err) {
-            res.status(STATUS_CODES.BAD_REQUEST).send(err.message);
+            if (err instanceof MissingParameterError) {
+                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
+                return;
+            }
+            if (err instanceof InvalidParameterError) {
+                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
+                return;
+            }
+            if (err instanceof DuplicateEntityError) {
+                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
+                return;
+            }
+            console.log(err);
+            res.sendStatus(STATUS_CODES.INTERNAL_SERVER_ERROR);
         }
     }
 
     delete(req, res) {
         try {
-            const deleted = this.patientsService.delete(req.params.phone);
+            const deleted = this.patientsService.deleteByPhone(req.params.phone);
+            if (!deleted) {
+                res.sendStatus(STATUS_CODES.NOT_FOUND);
+                return;
+            }
             res.json(deleted);
         } catch (err) {
-            res.status(STATUS_CODES.BAD_REQUEST).send(err.message);
+            if (err instanceof MissingParameterError) {
+                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
+                return;
+            }
+            if (err instanceof InvalidParameterError) {
+                res.status(STATUS_CODES.BAD_REQUEST).json({ error: { message: err.message } });
+                return;
+            }
+            console.log(err);
+            res.sendStatus(STATUS_CODES.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/services/patients-service.mjs
+++ src/services/patients-service.mjs
@@ -15,7 +15,7 @@ export class PatientsService {
         const { firstName, phone } = patientData;
 
         if (!phone) {
-            throw new MissingParameterError(ERRORS.MISSING_PARAMETER.replace('%s', 'phone'))
+            throw new MissingParameterError(ERRORS.MISSING_PARAMETER.replace('%s', 'phone'));
         }
 
         const isValidPhone = this.PHONE_REGEX.test(phone);
@@ -42,33 +42,48 @@ export class PatientsService {
         return this.patientsRepository.getAll();
     }
 
-    update(oldPhone, newData) {
-        const patient = this.patientsRepository.getOne(oldPhone);
-        
-        if (!patient) {
-            throw new Error(`patient with phone [${oldPhone}] not exists`);
+    update(newData) {
+        const { oldPhone, phone, firstName } = newData;
+
+        if (!oldPhone || !phone) {
+            throw new MissingParameterError(ERRORS.MISSING_PARAMETER.replace('%s', 'phone or oldPhone'));
         }
 
-        const isValidPhone = this.PHONE_REGEX.test(patient.phone);
-        if (!isValidPhone) {
-            throw new Error(`invalid phone format [${patient.phone}], E.164 expected`);
+        const patient = this.patientsRepository.getOne(oldPhone);
+
+        if (!patient) {
+            return;
         }        
 
-        patient.firstName = newData.firstName ?? patient.firstName;
-        patient.phone = newData.phone ?? patient.phone;
+        const isValidPhone = this.PHONE_REGEX.test(oldPhone) && this.PHONE_REGEX.test(phone);
+        if (!isValidPhone) {
+            throw new InvalidParameterError(ERRORS.INVALID_PHONE_FORMAT.replace('%s', `${oldPhone} or ${phone}`));
+        }
+
+        const isPatientExists = this.patientsRepository.getAll().some(p => p.phone === phone);
+        if (isPatientExists) {
+            throw new DuplicateEntityError(ERRORS.ENTITY_ALREADY_EXISTS.replace('%s', phone));
+        }
+
+        patient.firstName = firstName ?? patient.firstName;
+        patient.phone = phone;
 
         const updated = this.patientsRepository.update(oldPhone, patient);
 
         return updated;
     }
 
-    delete(phone) {
-        const deleted = this.patientsRepository.delete(phone);
-
-        if (!deleted) {
-            throw new Error(`Patient with phone ${phone} not found`);
+    deleteByPhone(phone) {
+        if (!phone) {
+            throw new MissingParameterError(ERRORS.MISSING_PARAMETER.replace('%s', 'phone'));
         }
 
+        const isValidPhone = this.PHONE_REGEX.test(phone);
+        if (!isValidPhone) {
+            throw new InvalidParameterError(ERRORS.INVALID_PHONE_FORMAT.replace('%s', phone));
+        }
+
+        const deleted = this.patientsRepository.delete(phone);
         return deleted;
     }
 }
