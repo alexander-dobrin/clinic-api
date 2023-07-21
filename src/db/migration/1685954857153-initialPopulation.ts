3234737f2a535e72ba3c23447c941f5d11e6a5b8
alexander-dobrin <alexander.dobrin@icloud.com>
2023-07-21 09:57:14 +0300

    refactor(db): use loop in migration script

--- src/db/migration/1685954857153-initialPopulation.ts
+++ src/db/migration/1685954857153-initialPopulation.ts
@@ -4,23 +4,22 @@ import { SALT_ROUNDS } from '../../common/constants';
 
 export class InitialPopulation1685954857153 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		queryRunner.query(
-			`
-                INSERT INTO "user" (first_name, "role", email, password)
-                VALUES
-                ('Mary', 'doctor', 'mary@gmail.com', '${hashSync('mary', SALT_ROUNDS)}'),
-                ('Alex', 'patient', 'alex@gmail.com', '${hashSync('alex', SALT_ROUNDS)}'),
-                ('Guest', 'guest', 'guest@gmail.com', '${hashSync('guest', SALT_ROUNDS)}');
-            `,
-		);
+		const users = [
+			{ first_name: 'Mary', role: 'doctor', email: 'mary@gmail.com', password: 'mary' },
+			{ first_name: 'Alex', role: 'patient', email: 'alex@gmail.com', password: 'alex' },
+			{ first_name: 'Guest', role: 'guest', email: 'guest@gmail.com', password: 'guest' },
+		];
+
+		for (const user of users) {
+			user.password = hashSync(user.password, SALT_ROUNDS);
+			const query = `
+				INSERT INTO "user" (first_name, "role", email, password)
+				VALUES ('${user.first_name}', '${user.role}', '${user.email}', '${user.password}');
+      		`;
+
+			await queryRunner.query(query);
+		}
 	}
 
-	public async down(queryRunner: QueryRunner): Promise<void> {
-		queryRunner.query(
-			`
-                DELETE FROM "user"
-                WHERE email IN ('mary@gmail.com', 'alex@gmail.com', 'guest@gmail.com');
-            `,
-		);
-	}
+	public async down(queryRunner: QueryRunner): Promise<void> {}
 }
